e1c29d3b2e068dfca088ba17bf35cf64170e65bb
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-12-01 15:03:27 +0100

    prints arrays in a table
    If parking value is true, it prints yes, otherwise no

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<?php 
+  $hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+  ];
+?>
+  
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +53,36 @@
   <title>Hotel</title>
 </head>
 <body>
-  <h1>Ciao</h1>
+  <h1>Elenco Hotel</h1>
+  <table class="table">
+  <thead>
+    <tr>
+      <?php foreach($hotels[0] as $key => $value) : ?>
+      <th scope="col"><?php echo ucfirst($key) ?></th>
+      <?php endforeach ?>
+    </tr>
+  </thead>
+  <tbody>
+    <?php foreach($hotels as $hotel) : ?>
+      <tr>
+        <?php foreach($hotel as $key => $value) : ?>
+          <td scope="row">
+            <?php if($key === 'parking') {
+              if ($value) {
+                echo 'Si';
+              } else {
+                echo 'No';
+              }
+            } else {
+              echo $value;
+            }
+            ?>
+          </td>
+        <?php endforeach ?>
+      </tr>
+    <?php endforeach ?>
+  </tbody>
+</table>
   
 </body>
 </html>
